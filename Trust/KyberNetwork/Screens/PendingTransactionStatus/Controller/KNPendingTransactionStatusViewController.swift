29cd3b0fac41de945fb639a0117ec08f181a157f
manhlx <manhlx.300695@gmail.com>
2018-03-17 18:24:39 +0800

    (FEAT) Add pending transaction status view to show status of a transaction after submitted

---
+++ Trust/KyberNetwork/Screens/PendingTransactionStatus/Controller/KNPendingTransactionStatusViewController.swift
@@ -0,0 +1,12 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+
+class KNPendingTransactionStatusViewController: UIViewController {
+
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = UIColor.clear
+  }
+}
