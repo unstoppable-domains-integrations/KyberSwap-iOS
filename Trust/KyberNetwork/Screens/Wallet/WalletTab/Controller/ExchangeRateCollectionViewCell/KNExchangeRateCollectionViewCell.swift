d35c0ce8236ce5a5314ee88e791eccf42828db1c
manhlx <manhlx.300695@gmail.com>
2018-04-04 17:45:54 +0800

    (FEAT) Create exchange rate collection view cell

---
+++ Trust/KyberNetwork/Screens/Wallet/WalletTab/Controller/ExchangeRateCollectionViewCell/KNExchangeRateCollectionViewCell.swift
@@ -0,0 +1,39 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+
+class KNExchangeRateCollectionViewCell: UICollectionViewCell {
+
+  static let cellID: String = "kExchangeRateCollectionViewCell"
+  static let cellHeight: CGFloat = 60.0
+  static let cellWidth: CGFloat = 120.0
+
+  @IBOutlet weak var tokenSymbolLabel: UILabel!
+  @IBOutlet weak var exchangeRateLabel: UILabel!
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    self.tokenSymbolLabel.text = ""
+    self.exchangeRateLabel.text = ""
+  }
+
+  func updateCell(with source: KNToken, dest: KNToken?) {
+    if let dest = dest {
+      // between 2 tokens
+      self.tokenSymbolLabel.text = "\(source.symbol)/\(dest.symbol)"
+      if let exchangeRate = KNRateCoordinator.shared.getRate(from: source, to: dest) {
+        self.exchangeRateLabel.text = EtherNumberFormatter.short.string(from: exchangeRate.rate, decimals: dest.decimal)
+      } else {
+        self.exchangeRateLabel.text = "-.-"
+      }
+    } else {
+      //source and USD
+      self.tokenSymbolLabel.text = "\(source.symbol)/USD"
+      if let exchangeRate = KNRateCoordinator.shared.usdRate(for: source) {
+        self.exchangeRateLabel.text = exchangeRate.displayRate
+      } else {
+        self.exchangeRateLabel.text = "-.-"
+      }
+    }
+  }
+}
