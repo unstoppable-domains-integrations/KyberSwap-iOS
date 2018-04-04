1369657270bcd216c45389eb4093a1b77654258a
manhlx <manhlx.300695@gmail.com>
2018-04-04 19:28:46 +0800

    (FEAT) Create transaction collection view cell

---
+++ Trust/KyberNetwork/Screens/History/Controller/TransactionCollectionViewCell/KNTransactionCollectionViewCell.swift
@@ -0,0 +1,9 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+
+class KNTransactionCollectionViewCell: UICollectionViewCell {
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+}
