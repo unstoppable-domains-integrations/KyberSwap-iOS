74a3ab0f626a850debf4f8900d9ca71d09c62851
manhlx <manhlx.300695@gmail.com>
2018-04-27 13:00:18 +0800

    (FEAT) Create initialization function for token transaction

--- Trust/KyberNetwork/Models/KNTokenTransaction.swift
+++ Trust/KyberNetwork/Models/KNTokenTransaction.swift
@@ -27,6 +27,33 @@ class KNTokenTransaction: Object {
   @objc dynamic var input: String = ""
   @objc dynamic var confirmations: String = ""
 
+  convenience init(dictionary: JSONDictionary) throws {
+    self.init()
+    self.id = try kn_cast(dictionary["hash"])
+    self.blockNumber = try kn_cast(dictionary["blockNumber"])
+    self.timeStamp = try kn_cast(dictionary["timeStamp"])
+    self.nonce = try kn_cast(dictionary["nonce"])
+    self.blockHash = try kn_cast(dictionary["blockHash"])
+    self.from = try kn_cast(dictionary["from"])
+    self.contractAddress = try kn_cast(dictionary["contractAddress"])
+    self.to = try kn_cast(dictionary["to"])
+    self.value = try kn_cast(dictionary["value"])
+    self.tokenName = try kn_cast(dictionary["tokenName"])
+    self.tokenSymbol = try kn_cast(dictionary["tokenSymbol"])
+    self.tokenDecimal = try kn_cast(dictionary["tokenDecimal"])
+    self.transactionIndex = try kn_cast(dictionary["transactionIndex"])
+    self.gas = try kn_cast(dictionary["gas"])
+    self.gasPrice = try kn_cast(dictionary["gasPrice"])
+    self.gasUsed = try kn_cast(dictionary["gasUsed"])
+    self.cumulativeGasUsed = try kn_cast(dictionary["cumulativeGasUsed"])
+    self.input = try kn_cast(dictionary["input"])
+    self.confirmations = try kn_cast(dictionary["confirmations"])
+  }
+
+  var date: Date {
+    return Date(timeIntervalSince1970: Double(timeStamp)!)
+  }
+
   override static func primaryKey() -> String? {
     return "id"
   }
