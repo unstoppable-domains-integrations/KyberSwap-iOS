7e20c6b7934c63689a467cc042f69d1e589f5ef3
manhlx <manhlx.300695@gmail.com>
2018-04-28 17:52:13 +0800

    (FEAT) Add wallet object and wallet storage to handle custom wallet data in Kyber app, allow user to set nickname for each wallet

---
+++ Trust/KyberNetwork/Models/KNWalletObject.swift
@@ -0,0 +1,22 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+import RealmSwift
+
+class KNWallet: Object {
+
+  @objc dynamic var address: String = ""
+  @objc dynamic var name: String = ""
+  @objc dynamic var date: Date = Date()
+
+  convenience init(address: String) {
+    self.init()
+    self.address = address
+    self.name = "Untitled"
+    self.date = Date()
+  }
+
+  override class func primaryKey() -> String? {
+    return "address"
+  }
+}
