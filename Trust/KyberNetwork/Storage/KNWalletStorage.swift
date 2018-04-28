7c9be5a2b9e5e4a041e79bd1f8298d0ca65f948b
manhlx <manhlx.300695@gmail.com>
2018-04-28 18:15:30 +0800

    (FEAT) Add functionalities for KNWalletStorage

--- Trust/KyberNetwork/Storage/KNWalletStorage.swift
+++ Trust/KyberNetwork/Storage/KNWalletStorage.swift
@@ -1,7 +1,48 @@
 // Copyright SIX DAY LLC. All rights reserved.
 
-import UIKit
+import RealmSwift
+import TrustKeystore
+import BigInt
 
-class KNWalletStorage: NSObject {
+class KNWalletStorage {
 
+  static var shared = KNWalletStorage()
+  private(set) var realm: Realm!
+
+  init() {
+    let config = RealmConfiguration.globalConfiguration(for: KNEnvironment.default.chainID)
+    self.realm = try! Realm(configuration: config)
+  }
+
+  var wallets: [KNWalletObject] {
+    return self.realm.objects(KNWalletObject.self)
+      .sorted(byKeyPath: "date", ascending: true)
+      .filter { !$0.address.isEmpty }
+  }
+
+  func get(forPrimaryKey key: String) -> KNWalletObject? {
+    return self.realm.object(ofType: KNWalletObject.self, forPrimaryKey: key)
+  }
+
+  func add(wallets: [KNWalletObject]) {
+    self.realm.beginWrite()
+    self.realm.add(wallets, update: true)
+    try!self.realm.commitWrite()
+  }
+
+  func update(wallets: [KNWalletObject]) {
+    self.add(wallets: wallets)
+  }
+
+  func delete(wallets: [KNWalletObject]) {
+    self.realm.beginWrite()
+    self.realm.delete(wallets)
+    try! self.realm.commitWrite()
+  }
+
+  func deleteAll() {
+    try! realm.write {
+      realm.delete(realm.objects(KNWalletObject.self))
+    }
+  }
 }
