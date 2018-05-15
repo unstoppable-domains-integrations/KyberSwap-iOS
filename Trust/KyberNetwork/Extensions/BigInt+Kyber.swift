9638d623b5424c093661893e6f57507c6a3d38e7
manhlx <manhlx.300695@gmail.com>
2018-05-15 16:43:24 +0700

    (FEAT) Add max fraction digits for short string from BigInt

--- Trust/KyberNetwork/Extensions/BigInt+Kyber.swift
+++ Trust/KyberNetwork/Extensions/BigInt+Kyber.swift
@@ -4,12 +4,16 @@ import BigInt
 
 extension BigInt {
 
-  func shortString(units: EthereumUnit) -> String {
-    return EtherNumberFormatter.short.string(from: self, units: units)
+  func shortString(units: EthereumUnit, maxFractionDigits: Int = 5) -> String {
+    let formatter = EtherNumberFormatter.short
+    formatter.maximumFractionDigits = maxFractionDigits
+    return formatter.string(from: self, units: units)
   }
 
-  func shortString(decimals: Int) -> String {
-    return EtherNumberFormatter.short.string(from: self, decimals: decimals)
+  func shortString(decimals: Int, maxFractionDigits: Int = 5) -> String {
+    let formatter = EtherNumberFormatter.short
+    formatter.maximumFractionDigits = maxFractionDigits
+    return formatter.string(from: self, decimals: decimals)
   }
 
   func fullString(units: EthereumUnit) -> String {
