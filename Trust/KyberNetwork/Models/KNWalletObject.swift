598b94237ff21971f4191a5d44cf73d0d4ad3cd7
manhlx <manhlx.300695@gmail.com>
2018-05-21 10:32:40 +0700

    (FEAT) Add backed up field into wallet object

--- Trust/KyberNetwork/Models/KNWalletObject.swift
+++ Trust/KyberNetwork/Models/KNWalletObject.swift
@@ -8,24 +8,27 @@ class KNWalletObject: Object {
   @objc dynamic var address: String = ""
   @objc dynamic var name: String = ""
   @objc dynamic var icon: String = ""
+  @objc dynamic var isBackedUp: Bool = true
   @objc dynamic var date: Date = Date()
 
-  convenience init(address: String, name: String = "Untitled") {
+  convenience init(address: String, name: String = "Untitled", isBackedUp: Bool = true) {
     self.init()
     self.address = address
     // TODO: Implement it
     self.name = name
     self.icon = "wallet_icon_\(arc4random() % 6)"
     self.date = Date()
+    self.isBackedUp = isBackedUp
   }
 
-  convenience init(address: String, name: String, icon: String, date: Date) {
+  convenience init(address: String, name: String, icon: String, date: Date, isBackedUp: Bool = true) {
     self.init()
     self.address = address
     // TODO: Implement it
     self.name = name
     self.icon = icon
     self.date = date
+    self.isBackedUp = isBackedUp
   }
 
   func copy(withNewName newName: String) -> KNWalletObject {
@@ -33,7 +36,8 @@ class KNWalletObject: Object {
       address: self.address,
       name: newName,
       icon: self.icon,
-      date: self.date
+      date: self.date,
+      isBackedUp: self.isBackedUp
     )
   }
 
