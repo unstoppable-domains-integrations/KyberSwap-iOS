92889dad203c138a62f9a528f4ee3635d3acd7d5
manhlx <manhlx.300695@gmail.com>
2018-06-20 12:06:39 +0700

    (FEAT) Add getting access token, user info and sign transaction data for buying IEO

--- Trust/KyberNetwork/Networking/Internal/KyberNetworkService.swift
+++ Trust/KyberNetwork/Networking/Internal/KyberNetworkService.swift
@@ -1,6 +1,7 @@
 // Copyright SIX DAY LLC. All rights reserved.
 
 import Moya
+import CryptoSwift
 
 enum KyberNetworkService: String {
   case getRate = "/getRate"
@@ -101,22 +102,80 @@ extension KNTrackerService: TargetType {
 
 enum KyberGOService {
   case listIEOs
+  case getAccessToken(code: String)
+  case getUserInfo(accessToken: String)
+  case getSignedTx(userID: Int, ieoID: Int, address: String, time: UInt)
 }
 
 extension KyberGOService: TargetType {
   var baseURL: URL {
-    return URL(string: "https://kyber.mangcut.vn/api/ieos")!
+    switch self {
+    case .listIEOs:
+      return URL(string: "https://kyber.mangcut.vn/api/ieos")!
+    case .getAccessToken:
+      return URL(string: "https://kyber.mangcut.vn/oauth/token")!
+    case .getUserInfo:
+      return URL(string: "https://kyber.mangcut.vn/api/user_info")!
+    case .getSignedTx:
+      return URL(string: KNSecret.ieoSignedEndpoint)!
+    }
   }
 
   var path: String { return "" }
-  var method: Moya.Method { return .get }
-  var task: Task { return .requestPlain }
+
+  var method: Moya.Method {
+    switch self {
+    case .listIEOs, .getUserInfo: return .get
+    default: return .post
+    }
+  }
+
+  var task: Task {
+    switch self {
+    case .listIEOs: return .requestPlain
+    case .getAccessToken(let code):
+      let json: JSONDictionary = [
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": KNSecret.redirectURL,
+        "client_id": KNSecret.appID,
+        "client_secret": KNSecret.secret,
+      ]
+      let data = try! JSONSerialization.data(withJSONObject: json, options: [])
+      return .requestData(data)
+    case .getUserInfo(let accessToken):
+      let json: JSONDictionary = [ "access_token": accessToken ]
+      let data = try! JSONSerialization.data(withJSONObject: json, options: [])
+      return .requestData(data)
+    case .getSignedTx(let userID, let ieoID, let address, let time):
+      let params: JSONDictionary = [
+        "contributor": address,
+        "ieoid": ieoID,
+        "nonce": time,
+        "userid": userID,
+      ]
+      return .requestCompositeData(bodyData: Data(), urlParameters: params)
+    }
+  }
   var sampleData: Data { return Data() }
   var headers: [String: String]? {
-    return [
-      "content-type": "application/json",
-      "client": Bundle.main.bundleIdentifier ?? "",
-      "client-build": Bundle.main.buildNumber ?? "",
-    ]
+    switch self {
+    case .getSignedTx(let userID, let ieoID, let address, let time):
+      let string = "contributor=\(address)&ieoid=\(ieoID)&nonce=\(time)&userid=\(userID)"
+      let hmac = try! HMAC(key: KNSecret.ieoSignedKey, variant: .sha512)
+      let hash = try! hmac.authenticate(string.bytes).toHexString()
+      return [
+        "Content-Type": "application/x-www-form-urlencoded",
+        "signed": hash,
+        "client": Bundle.main.bundleIdentifier ?? "",
+        "client-build": Bundle.main.buildNumber ?? "",
+      ]
+    default:
+      return [
+        "content-type": "application/json",
+        "client": Bundle.main.bundleIdentifier ?? "",
+        "client-build": Bundle.main.buildNumber ?? "",
+      ]
+    }
   }
 }
