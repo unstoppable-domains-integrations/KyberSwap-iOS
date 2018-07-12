e206b6ead694f082cfb5dfc2b22cd865de4cc49f
manhlx <manhlx.300695@gmail.com>
2018-07-12 11:03:45 +0700

    (FEAT) Add button show/hide secure text for importing private key,git st

--- Trust/KyberNetwork/Screens/Wallet/Import/Controller/ImpoprtPrivateKey/KNImportPrivateKeyViewController.swift
+++ Trust/KyberNetwork/Screens/Wallet/Import/Controller/ImpoprtPrivateKey/KNImportPrivateKeyViewController.swift
@@ -11,8 +11,11 @@ class KNImportPrivateKeyViewController: KNBaseViewController {
 
   weak var delegate: KNImportPrivateKeyViewControllerDelegate?
 
+  private var isSecureText: Bool = true
+  @IBOutlet weak var secureTextButton: UIButton!
   @IBOutlet weak var enterPrivateKeyTextLabel: UILabel!
   @IBOutlet weak var enterPrivateKeyTextField: UITextField!
+  @IBOutlet weak var privateKeyNoteLabel: UILabel!
 
   @IBOutlet weak var nextButton: UIButton!
 
@@ -23,7 +26,7 @@ class KNImportPrivateKeyViewController: KNBaseViewController {
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    self.enterPrivateKeyTextField.text = ""
+    self.resetUI()
   }
 
   fileprivate func setupUI() {
@@ -35,6 +38,21 @@ class KNImportPrivateKeyViewController: KNBaseViewController {
     self.enterPrivateKeyTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 0))
 
     self.nextButton.rounded(radius: 4.0)
+    self.privateKeyNoteLabel.text = "Private key has to be 64 characaters".toBeLocalised()
+
+    self.resetUI()
+  }
+
+  fileprivate func resetUI() {
+    self.enterPrivateKeyTextField.text = ""
+    self.isSecureText = true
+    self.updateSecureTextEntry()
+  }
+
+  fileprivate func updateSecureTextEntry() {
+    let secureTextImage = UIImage(named: self.isSecureText ? "hide_secure_text" : "show_secure_text")
+    self.secureTextButton.setImage(secureTextImage, for: .normal)
+    self.enterPrivateKeyTextField.isSecureTextEntry = self.isSecureText
   }
 
   @IBAction func qrCodeButtonPressed(_ sender: Any) {
@@ -43,19 +61,24 @@ class KNImportPrivateKeyViewController: KNBaseViewController {
     self.parent?.present(qrcodeReader, animated: true, completion: nil)
   }
 
+  @IBAction func secureTextButtonPressed(_ sender: Any) {
+    self.isSecureText = !self.isSecureText
+    self.updateSecureTextEntry()
+  }
+
   @IBAction func nextButtonPressed(_ sender: Any) {
     let privateKey: String = self.enterPrivateKeyTextField.text ?? ""
-    if privateKey.isEmpty {
-      self.showWarningTopBannerMessage(
-        with: "Field Required".toBeLocalised(),
-        message: "Please check your input data again.".toBeLocalised()
+    guard privateKey.count == 64 else {
+      self.showErrorTopBannerMessage(
+        with: "Invalid input".toBeLocalised(),
+        message: "Private key should have 64 characters".toBeLocalised()
       )
-    } else {
-      self.delegate?.importPrivateKeyViewControllerDidPressNext(
-        sender: self,
-        privateKey: privateKey
-      )
+      return
     }
+    self.delegate?.importPrivateKeyViewControllerDidPressNext(
+      sender: self,
+      privateKey: privateKey
+    )
   }
 }
 
