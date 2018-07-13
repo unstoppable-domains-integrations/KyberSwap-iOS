2533f11ed78cbff871766fc3277d9789c8bf5d97
manhlx <manhlx.300695@gmail.com>
2018-07-13 11:24:53 +0700

    (FEAT) Use Trust public tokens repo to retrieve token icon

--- Trust/KyberNetwork/Screens/CommonViews/Token/SearchToken/TableViewCell/KNSearchTokenTableViewCell.swift
+++ Trust/KyberNetwork/Screens/CommonViews/Token/SearchToken/TableViewCell/KNSearchTokenTableViewCell.swift
@@ -18,7 +18,15 @@ class KNSearchTokenTableViewCell: UITableViewCell {
 
   func updateCell(with token: TokenObject) {
     //TODO: remove default value
-    self.iconImageView.image = UIImage(named: token.icon) ?? UIImage(named: "default_token")
+    if let image = UIImage(named: token.icon) {
+      self.iconImageView.image = image
+    } else {
+      // Token image from Trust public repo
+      let url = "https://raw.githubusercontent.com/TrustWallet/tokens/master/images/\(token.contract.lowercased()).png"
+      self.iconImageView.setImage(
+        with: url,
+        placeholder: UIImage(named: "default_token"))
+    }
     self.tokenSymbolLabel.text = token.symbol
     self.tokenNameLabel.text = token.name
     self.layoutIfNeeded()
