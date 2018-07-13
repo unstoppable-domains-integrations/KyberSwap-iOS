7e891a18b07383a765a5757ff8be23d9ea1c43dc
manhlx <manhlx.300695@gmail.com>
2018-07-13 13:04:16 +0700

    (FEAT) Add extension for UIImage and UIButton for loading token icon image

---
+++ Trust/KyberNetwork/Extensions/UIButton+Kyber.swift
@@ -0,0 +1,55 @@
+// Copyright SIX DAY LLC. All rights reserved.
+
+import UIKit
+
+extension UIButton {
+  func setImage(
+    with url: URL,
+    placeHolder: UIImage?,
+    size: CGSize? = nil,
+    state: UIControlState = .normal
+    ) {
+    self.setImage(placeHolder?.resizeImage(to: size), for: state)
+    URLSession.shared.dataTask(with: url) { (data, _, error) in
+      if error == nil, let data = data, let image = UIImage(data: data) {
+        DispatchQueue.main.async {
+          self.setImage(image.resizeImage(to: size), for: .normal)
+        }
+      }
+    }.resume()
+  }
+
+  func setImage(
+    with string: String,
+    placeHolder: UIImage?,
+    size: CGSize? = nil,
+    state: UIControlState = .normal
+    ) {
+    self.setImage(placeHolder?.resizeImage(to: size), for: state)
+    guard let url = URL(string: string) else { return }
+    self.setImage(
+      with: url,
+      placeHolder: placeHolder,
+      size: size,
+      state: state
+    )
+  }
+
+  func setTokenImage(
+    token: TokenObject,
+    size: CGSize? = nil,
+    state: UIControlState = .normal
+    ) {
+    if let image = UIImage(named: token.icon) {
+      self.setImage(image.resizeImage(to: size), for: .normal)
+    } else {
+      let placeHolderImg = UIImage(named: "default_token")
+      self.setImage(
+        with: token.iconURL,
+        placeHolder: placeHolderImg,
+        size: size,
+        state: state
+      )
+    }
+  }
+}
--- Trust/KyberNetwork/Extensions/UIImage+Kyber.swift
+++ Trust/KyberNetwork/Extensions/UIImage+Kyber.swift
@@ -16,4 +16,18 @@ extension UIImage {
     }
     return nil
   }
+
+  func resizeImage(to newSize: CGSize?) -> UIImage? {
+    guard let size = newSize else { return self }
+    if self.size == size { return self }
+
+    let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+
+    UIGraphicsBeginImageContextWithOptions(size, false, 0)
+    self.draw(in: rect)
+    let newImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    return newImage
+  }
 }
