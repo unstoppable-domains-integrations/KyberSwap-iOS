8b64e411e505f151de255301c4b41459f709e5ec
manhlx <manhlx.300695@gmail.com>
2018-08-10 16:06:49 +0700

    (FEAT) Add drawing dashline in view

--- Trust/KyberNetwork/Extensions/UIView+Kyber.swift
+++ Trust/KyberNetwork/Extensions/UIView+Kyber.swift
@@ -152,4 +152,18 @@ extension UIView {
     self.layer.addSublayer(border)
     self.layer.masksToBounds = true
   }
+
+  func dashLine(width: CGFloat, color: UIColor) {
+    let shapeLayer = CAShapeLayer()
+    shapeLayer.strokeColor = color.cgColor
+    shapeLayer.lineWidth = width
+    shapeLayer.lineDashPattern = [7, 3] // 7: length of dash, 3: length of gap between
+
+    let path = CGMutablePath()
+    let start = CGPoint(x: self.frame.minX, y: self.center.y)
+    let end = CGPoint(x: self.frame.maxX, y: self.center.y)
+    path.addLines(between: [start, end])
+    shapeLayer.path = path
+    self.layer.addSublayer(shapeLayer)
+  }
 }
