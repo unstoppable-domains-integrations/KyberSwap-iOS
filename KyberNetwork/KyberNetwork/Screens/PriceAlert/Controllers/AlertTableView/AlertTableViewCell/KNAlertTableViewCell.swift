c891abb4c595509c48621ba4f723b6a8b91e5770
manhlx <manhlx.300695@gmail.com>
2019-03-19 13:43:09 +0700

    Using 2 lines for triggered time

--- KyberNetwork/KyberNetwork/Screens/PriceAlert/Controllers/AlertTableView/AlertTableViewCell/KNAlertTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/PriceAlert/Controllers/AlertTableView/AlertTableViewCell/KNAlertTableViewCell.swift
@@ -47,7 +47,7 @@ class KNAlertTableViewCell: UITableViewCell {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "HH:mm dd/MM/yyyy"
         let triggerString = String(
-          format: "Triggered: %@".toBeLocalised(),
+          format: "Triggered: \n%@".toBeLocalised(),
           dateFormatter.string(from: Date(timeIntervalSince1970: alert.triggeredDate))
         )
         attributedString.append(NSAttributedString(string: "\n\(triggerString)", attributes: triggerAttributes))
