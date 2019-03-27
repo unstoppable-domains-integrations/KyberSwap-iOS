c96baa2cd5783d4ea1d28bffef2b3f7f09fc2973
manhlx <manhlx.300695@gmail.com>
2019-03-27 16:57:58 +0700

    Add rate our app on AppStore

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/SettingsVC/Coordinator/KNSettingsCoordinator.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Settings/SettingsVC/Coordinator/KNSettingsCoordinator.swift
@@ -182,7 +182,7 @@ extension KNSettingsCoordinator: KNSettingsTabViewControllerDelegate {
       self.navigationController.openSafari(with: "https://goo.gl/forms/ZarhiV7MPE0mqr712")
     case .rateOurApp:
       KNCrashlyticsUtil.logCustomEvent(withName: "settings", customAttributes: ["value": "rate_our_app"])
-      self.navigationController.openSafari(with: "https://goo.gl/forms/RkvyT7MpTYV93gV02")
+      self.navigationController.openSafari(with: "https://apple.co/2USHOtx")
     }
   }
 
