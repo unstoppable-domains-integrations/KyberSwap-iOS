8af25b8cdd411ed813176be720e6e465de93bca6
manhlx <manhlx.300695@gmail.com>
2019-04-08 12:58:58 +0700

    Broadcast transaction to multiple nodes

--- KyberNetwork/EtherClient/EtherServiceRequest.swift
+++ KyberNetwork/EtherClient/EtherServiceRequest.swift
@@ -34,3 +34,65 @@ struct EtherServiceRequest<Batch: JSONRPCKit.Batch>: APIKit.Request {
         return try batch.responses(from: object)
     }
 }
+
+struct EtherServiceKyberRequest<Batch: JSONRPCKit.Batch>: APIKit.Request {
+  let batch: Batch
+
+  typealias Response = Batch.Responses
+
+  var baseURL: URL {
+    // Change to KyberNetwork endpoint
+    if let customRPC = KNEnvironment.default.customRPC, let path = URL(string: customRPC.endpointKyber + KNEnvironment.default.nodeEndpoint) {
+      return path
+    }
+    let config = Config()
+    return config.rpcURL
+  }
+
+  var method: HTTPMethod {
+    return .post
+  }
+
+  var path: String {
+    return ""
+  }
+
+  var parameters: Any? {
+    return batch.requestObject
+  }
+
+  func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+    return try batch.responses(from: object)
+  }
+}
+
+struct EtherServiceAlchemyRequest<Batch: JSONRPCKit.Batch>: APIKit.Request {
+  let batch: Batch
+
+  typealias Response = Batch.Responses
+
+  var baseURL: URL {
+    // Change to KyberNetwork endpoint
+    if let customRPC = KNEnvironment.default.customRPC, let path = URL(string: customRPC.endpointAlchemy + KNEnvironment.default.nodeEndpoint) {
+      return path
+    }
+    let config = Config()
+    return config.rpcURL
+  }
+
+  var method: HTTPMethod {
+    return .post
+  }
+
+  var path: String {
+    return ""
+  }
+
+  var parameters: Any? {
+    return batch.requestObject
+  }
+
+  func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+    return try batch.responses(from: object)
+  }
+}
