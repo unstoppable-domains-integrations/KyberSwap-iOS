ba03fbccd13176eb6467fc94d13700b4973e42c0
manhlx <manhlx.300695@gmail.com>
2019-05-27 16:40:14 +0700

    Update limit order search token cell layout

--- KyberNetwork/KyberNetwork/Screens/Tabbars/LimitOrder/Controllers/SearchToken/Cell/KNLimitOrderTokenTableViewCell.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/LimitOrder/Controllers/SearchToken/Cell/KNLimitOrderTokenTableViewCell.swift
@@ -25,9 +25,13 @@ class KNLimitOrderTokenTableViewCell: UITableViewCell {
         with: token.iconURL,
         placeholder: UIImage(named: "default_token"))
     }
-    self.tokenSymbolLabel.text = (token.isWETH || token.isETH) && isSource ? "ETH*" : "\(token.symbol.prefix(8))"
+    let isETHStar = (token.isWETH || token.isETH) && isSource
+    self.tokenSymbolLabel.text = isETHStar ? "ETH*" : "\(token.symbol.prefix(8))"
+    self.tokenSymbolLabel.font = isETHStar ? UIFont.Kyber.semiBold(with: 14) : UIFont.Kyber.medium(with: 14)
     self.tokenSymbolLabel.addLetterSpacing()
-    self.tokenNameLabel.text =  (token.isWETH || token.isETH) && isSource ? "Ether that is compatible to ERC20 standard. 1 WETH is equal to 1 ETH. Limit Order works with WETH (not ETH).".toBeLocalised() : token.name
+    self.tokenNameLabel.text = isETHStar ? "Ether that is compatible to ERC20 standard. 1 WETH is equal to 1 ETH. Limit Order works with WETH (not ETH).".toBeLocalised() : token.name
+    self.tokenNameLabel.textColor = isETHStar ? UIColor(red: 20, green: 25, blue: 39) : UIColor(red: 158, green: 161, blue: 170)
+    self.tokenNameLabel.font = UIFont.Kyber.medium(with: 12, italic: isETHStar)
     self.tokenNameLabel.addLetterSpacing()
     let balText: String = balance?.string(
       decimals: token.decimals,
