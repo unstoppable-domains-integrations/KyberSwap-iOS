4d312a21984caeba37693ed1920aca4129a24986
manhlx <manhlx.300695@gmail.com>
2019-06-07 09:57:57 +0700

    Keep default pair when switch wallet

--- KyberNetwork/KyberNetwork/Screens/Tabbars/LimitOrder/Controllers/CreateOrder/KNCreateLimitOrderViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/LimitOrder/Controllers/CreateOrder/KNCreateLimitOrderViewController.swift
@@ -463,6 +463,9 @@ class KNCreateLimitOrderViewController: KNBaseViewController {
       self.updateRelatedOrdersView()
       self.rateContainerView.rounded(radius: 4.0)
       self.scrollContainerView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+      if sender != nil {
+        self.targetRateTextField.becomeFirstResponder()
+      }
       self.view.layoutIfNeeded()
     }
   }
