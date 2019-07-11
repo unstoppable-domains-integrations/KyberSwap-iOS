217c4c5661696b1c989fc3a573a7318bc17d9366
manhlx <manhlx.300695@gmail.com>
2019-07-11 21:42:46 +0700

    Increase build number 82 version 2.0.0

--- KyberNetwork/KyberNetwork/Screens/Splash/Controller/KNSplashScreenViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/Splash/Controller/KNSplashScreenViewController.swift
@@ -13,6 +13,7 @@ class KNSplashScreenViewController: KNBaseViewController {
     super.viewDidLoad()
     self.view.applyGradient(with: UIColor.Kyber.backgroundColors)
     self.debugInfoView.isHidden = true
+
     self.versionLabel.text = "Version: \(Bundle.main.versionNumber ?? "")"
     self.networkLabel.text = "Network: \(KNEnvironment.default.displayName)"
     self.splashLogoImageView.image = nil
