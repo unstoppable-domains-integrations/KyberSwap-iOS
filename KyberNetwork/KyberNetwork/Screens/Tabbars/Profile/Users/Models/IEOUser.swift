b4cfc65fc170b997d8c5212affee05dabed83edf
Quan <quan.ta@kyber.network>
2020-02-20 14:58:43 +0700

    - revert old schema to avoid crash when upgrading app

--- KyberNetwork/KyberNetwork/Screens/Tabbars/Profile/Users/Models/IEOUser.swift
+++ KyberNetwork/KyberNetwork/Screens/Tabbars/Profile/Users/Models/IEOUser.swift
@@ -9,6 +9,7 @@ class IEOUser: Object {
   @objc dynamic var name: String = ""
   @objc dynamic var contactType: String = ""
   @objc dynamic var contactID: String = ""
+  @objc dynamic var kycStatus: String = ""
   @objc dynamic var tokenType: String = ""
   @objc dynamic var expireTime: Double = 0
   @objc dynamic var accessToken: String = ""
@@ -35,6 +36,46 @@ class IEOUser: Object {
     self.accessToken = accessToken
     self.refreshToken = refreshToken
     self.expireTime = expireTime
+  }
+
+  override class func primaryKey() -> String? {
+    return "userID"
+  }
+}
+
+class UserKYCDetailsInfo: Object {
+  @objc dynamic var userID: Int = -1
+  @objc dynamic var rejectedReason: String = ""
+  @objc dynamic var firstName: String = ""
+  @objc dynamic var middleName: String = ""
+  @objc dynamic var lastName: String = ""
+  @objc dynamic var nativeFullName: String = ""
+  @objc dynamic var nationality: String = ""
+  @objc dynamic var residentialAddress: String = ""
+  @objc dynamic var country: String = ""
+  @objc dynamic var city: String = ""
+  @objc dynamic var zipCode: String = ""
+  @objc dynamic var documentProofAddress: String = ""
+  @objc dynamic var photoProofAddress: String = ""
+  @objc dynamic var sourceFund: String = ""
+  @objc dynamic var occupationCode: String = ""
+  @objc dynamic var industryCode: String = ""
+  @objc dynamic var taxResidencyCountry: String = ""
+  @objc dynamic var haveTaxIndentification: Bool = false
+  @objc dynamic var taxIDNUmber: String = ""
+  @objc dynamic var gender: Bool = true
+  @objc dynamic var dob: String = ""
+  @objc dynamic var documentType: String = ""
+  @objc dynamic var documentNumber: String = ""
+  @objc dynamic var documentPhotoFront: String = ""
+  @objc dynamic var documentPhotoBack: String = ""
+  @objc dynamic var documentIssueDate: String = ""
+  @objc dynamic var documentExpiryDate: String = ""
+  @objc dynamic var documentSelfiePhoto: String = ""
+
+  convenience init(userID: Int, dict: JSONDictionary, rejectReason: String) {
+    self.init()
+    
   }
 
   override class func primaryKey() -> String? {
