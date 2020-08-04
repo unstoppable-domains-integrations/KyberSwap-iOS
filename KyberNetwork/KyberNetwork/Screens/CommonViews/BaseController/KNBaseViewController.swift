7015e2b6ae78d9ffccfde82cd758895b9ba30872
MrQKyber <60604928+MrQKyber@users.noreply.github.com>
2020-08-04 15:01:27 +0700

    [IOS-97] Dismiss quick tutorial if needed when view did disappear #140

--- KyberNetwork/KyberNetwork/Screens/CommonViews/BaseController/KNBaseViewController.swift
+++ KyberNetwork/KyberNetwork/Screens/CommonViews/BaseController/KNBaseViewController.swift
@@ -14,6 +14,7 @@ class KNBaseViewController: UIViewController {
   override func viewDidDisappear(_ animated: Bool) {
     super.viewDidDisappear(animated)
     NSLog("Did dismiss: \(self.className)")
+    self.dismissTutorialOverlayer()
   }
 }
 
